af3abcf785066e777a927f7baf0b73dd77415a08
Lewis Scott <lscott19@mail.greenriver.edu>
2020-04-22 17:18:52 -0400

    updated index.php and view added

--- index.php
+++ index.php
@@ -14,7 +14,10 @@ $f3 = Base::instance();
 
 // Define a default route
 $f3->route('GET /', function () {
-    echo '<h1>It is raining today!</h1>';
+    //echo '<h1>It is raining today!</h1>';
+    $view = new Template();
+    echo $view->render
+    ('views/home.html');
 
 }
 );
